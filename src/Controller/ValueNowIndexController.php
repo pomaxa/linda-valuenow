fe7b9d3c4eeb36a0699a913c7c27022dda62f440
Artjoms Fedosenko <artjoms.fedosenko@gmail.com>
2022-07-06 20:34:47 +0300

    Create user and registration form

---
+++ src/Controller/ValueNowIndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ValueNowIndexController extends AbstractController
+{
+    #[Route('/', name: 'app_value_now_index')]
+    public function index(): Response
+    {
+        return $this->render('value_now_index/index.html.twig', [
+            'controller_name' => 'ValueNowIndexController',
+        ]);
+    }
+}
